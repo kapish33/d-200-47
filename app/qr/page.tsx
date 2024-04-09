d65c7f73b17057e81a2bf347de4dd94e5105b5ca
kapish33 <kapishsingh2209@gmial.com>
2024-04-09 22:25:21 +0530

    QR added

---
+++ app/qr/page.tsx
@@ -0,0 +1,9 @@
+import { crudOperations } from '@/firebase/firestore-db/crud';
+import { redirect } from 'next/navigation';
+
+export default async function qr() {
+  const url = await crudOperations('GET', 'cource');
+  // console.log('url', url);
+  redirect(url[0].url);
+  return <></>;
+}
