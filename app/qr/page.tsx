b35c23a788b15f87a751e9e3cab8a68117912f73
kapish33 <kapishsingh2209@gmial.com>
2024-04-09 22:33:19 +0530

    revalidate

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,6 +1,9 @@
 import { crudOperations } from '@/firebase/firestore-db/crud';
 import { redirect } from 'next/navigation';
 
+export const revalidate = 1;
+
+
 export default async function qr() {
   const url = await crudOperations('GET', 'cource');
   // console.log('url', url);
